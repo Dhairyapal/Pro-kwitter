d376b21e4edd1619e9a10854bb0a7dad6764d79b
Dhairyapal <78430771+Dhairyapal@users.noreply.github.com>
2021-09-04 12:40:38 +0530

    Add files via upload

---
+++ kwitter.room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDrC6T7jEjk9i6vPS-sJT8RBK0UhUcN-M",
+    authDomain: "kwitter-project-6085a.firebaseapp.com",
+    projectId: "kwitter-project-6085a",
+    storageBucket: "kwitter-project-6085a.appspot.com",
+    messagingSenderId: "999340331755",
+    appId: "1:999340331755:web:c52a85fe9c0a9575f864ae"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
